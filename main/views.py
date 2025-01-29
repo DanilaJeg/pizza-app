fe9e8cc53451a63a672d098e1098b2d9612083de
DanilaJeg <jegorenkovsd@gmail.com>
2025-01-29 19:20:53 +0000

    minor bug fix in order redirect

--- main/views.py
+++ main/views.py
@@ -12,7 +12,7 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             login(request, form.save())
-            return redirect("Order")
+            return redirect("order")
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {"form": form})
@@ -23,7 +23,7 @@ def login_view(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             login(request, form.get_user())
-            return redirect('Order')
+            return redirect('order')
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {"form": form})
