7c16107c23c11899ca7f5acead313c59a3a576ed
DanilaJeg <jegorenkovsd@gmail.com>
2025-01-29 18:35:07 +0000

    user registration and login pages

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import request
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def index(request):
@@ -7,3 +9,24 @@ def index(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            login(request, form.save())
+            return redirect("index")
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {"form": form})
