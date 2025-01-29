1b48d71f76ed079922dcfdb02ef583b06276af0f
DanilaJeg <jegorenkovsd@gmail.com>
2025-01-29 20:52:46 +0000

    Design all pizza models and added them to admin.py so admin can add toppings or other things of their choice

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Pizza, Size, Base, Cheese, Topping, Crust
 
 # Register your models here.
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ['size', 'price']
+    search_fields = ['size']
+
+@admin.register(Crust)
+class CrustAdmin(admin.ModelAdmin):
+    list_display = ['crust', 'price']
+    search_fiels = ['crust']
+
+@admin.register(Topping)
+class ToppingAdmin(admin.ModelAdmin):
+    list_display = ['topping', 'price']
+    search_fields = ['topping']
+
+@admin.register(Base)
+class BaseAdmin(admin.ModelAdmin):
+    list_display = ['base', 'price']
+    search_fields = ['base']
+
+@admin.register(Cheese)
+class CheeseAdmin(admin.ModelAdmin):
+    list_display = ['cheese', 'price']
+    search_fields = ['cheese']
+
+@admin.register(Pizza)
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ['size', 'crust', 'display_toppings', 'totalPrice']
+    search_fields = ['size__name', 'crust__name']
+
+    def display_toppings(self, obj):
+        return ", ".join([t.topping for t in obj.topping.all()])
+    display_toppings.short_description = "toppings"
--- main/models.py
+++ main/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+class Size(models.Model):
+    size = models.CharField(max_length=50)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.size
+
+class Crust(models.Model):
+    crust = models.CharField(max_length=50)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.crust
+
+class Base(models.Model):
+    base = models.CharField(max_length=50)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.base
+
+class Topping(models.Model):
+    topping = models.CharField(max_length=50)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.topping
+
+class Cheese(models.Model):
+    cheese = models.CharField(max_length=50)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.cheese
+
+class Pizza(models.Model):
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    crust = models.ForeignKey(Crust, on_delete=models.CASCADE)
+    base = models.ForeignKey(Base, on_delete=models.CASCADE)
+    topping = models.ManyToManyField(Topping, blank=True)
+    cheese = models.ForeignKey(Cheese, on_delete=models.CASCADE)
+
+    def totalPrice(self):
+        toppings = sum(t.price for t in self.topping.all())
+        return self.size.price + self.crust.price + self.base.price + toppings + self.cheese.price
+
+    def __str__(self):
+        return f"{self.size} pizza with {self.crust} crust and {self.base} base"
+    
